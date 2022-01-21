d2e4b0c9f3010b3f089fe9432c7d48ab2a400dae
Charly Joan Llanes Euan <cllanes@frd.org.mx>
2022-01-21 12:16:19 -0600

    stateWindow Function

--- instructions.py
+++ instructions.py
@@ -2,17 +2,20 @@
 from tkinter import *
 # Importing tkPDFViewer to place pdf file in gui. 
 from tkPDFViewer import tkPDFViewer as pdf 
+def stateWindow(window,state):
+    if state!=0:
+        languageWindow(window, state)
 def languageWindow(window, state):
     newWindow = Toplevel(window)
     newWindow.title("How to Play...")
     newWindow.geometry("670x500")
-    if state==True:
+    if state==1:
         vEnglish = pdf.ShowPdf() 
         vEnglishOpen = vEnglish.pdf_view(newWindow, 
                     pdf_location = r"MasterEnglish.pdf",  
                     width = 670, height = 500)
         vEnglishOpen.pack()
-    else:
+    elif state==2:
         vSpanish = pdf.ShowPdf() 
         vSpanishOpen = vSpanish.pdf_view(newWindow, 
                     pdf_location = r"MasterSpanish.pdf",  
